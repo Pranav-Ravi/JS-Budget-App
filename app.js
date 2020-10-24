d39cd95a27fe376b9f4cac9fbafa945cdb26ae47
iVAR <pranraviofficial@gmail.com>
2020-10-24 23:42:36 +0530

    Budget controller updates and UI updates
    * Budget UI controller gets updated
    * Event delegation notes
    * Created Delete Event Listener using Event Delegations
    * Budget UI bugs got fixed

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ var controller = (function(BudgetCtrl, UICtrl){
 
 
 
-//BUDGET CONTROLLER
+/**********************************BUDGET CONTROLLER********************************/
 var budgetController = (function() {
     
     /*IMP
@@ -68,10 +68,18 @@ var budgetController = (function() {
     */
 
     //private variables and functions
-    var Expenses = function(id, description, value) {
+    var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     };
 
     var Income = function(id, description, value) {
@@ -82,9 +90,10 @@ var budgetController = (function() {
     
     var calculateTotal = function(type) {
         var sum = 0;
-        data.allType[type].forEach(function (currElement) {
-            sum += currElement.value;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
         });
+        data.totals[type] = sum;
         /*
          For example,
          sum = 0
@@ -106,7 +115,7 @@ var budgetController = (function() {
             inc: 0
         },
         budget: 0,
-        percentage: -1; 
+        percentage: -1
     }
 
     //public varibles and functions
@@ -121,11 +130,10 @@ var budgetController = (function() {
             } else {
                 ID = 0;
             }
-            
 
             //create new item based on whether its a 'exp' or 'inc'
             if(type === 'exp') {
-                newItem = new Expenses(ID, des, val);
+                newItem = new Expense(ID, des, val);
             } else if(type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
@@ -172,26 +180,33 @@ var budgetController = (function() {
     };
 })();
 
-//USER INTERFACE CONTROLLER
+
+
+/*******************************************USER INTERFACE CONTROLLER****************************************************/
 var uiController = (function() {
 
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        addBtn: '.add__btn',
+        inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        budgetIncLabel: '.budget__income--value',
+        budgetExpLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container',
     };
 
 
     //public variables and functions
     return {
-        getInputValues: function() {
+        getInput: function() {
             return {
-                inputType: document.querySelector(DOMstrings.inputType).value, //will be either income or expense
-                inputDescription: document.querySelector(DOMstrings.inputDescription).value,
-                inputValue: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+                type: document.querySelector(DOMstrings.inputType).value, //will be either income or expense
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
 
@@ -199,22 +214,23 @@ var uiController = (function() {
             return DOMstrings;
         },
 
-        addItemUI: function(obj, type) {
+        addListItem: function(obj, type) {
             var html, newHtml, element;
 
             //create HTML string with placeholder text
             if(type==='inc') {
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-0">%id%<div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if(type==='exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-0">%id%<div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">%percentage%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
         
             //Replace the placeholder string with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
             newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%percentage%', obj.percentage);
 
             //Insert the html into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -234,26 +250,43 @@ var uiController = (function() {
             //using focus to put the cursor in the input line, even after pushing 
             //new value to the array
             fieldsArr[0].focus();
-        }
+        },
+
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.budgetIncLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.budgetExpLabel).textContent = obj.totalExp;
+
+            if(obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+            
+        },
     }
 
 })();
 
 
-//GLOBAL CONTROLLER
+/***************************************GLOBAL CONTROLLER************************************************/
 var controller = (function(budgetCtrl, uiCtrl){
+
     //PRIVATE VARIABLES AND FUNCTIONS
     var setupEventListeners = function() {
         var DOM = uiCtrl.getDOMstrings();
 
         //add__btn event
-        document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem);
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
         //enter key press event
         document.addEventListener('keypress', function(event) {
             if(event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
+                console.log('Item has been added!.');
             }
         });
+        //event delegate
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     }
 
     var updateBudget = function() {
@@ -264,35 +297,60 @@ var controller = (function(budgetCtrl, uiCtrl){
         var budget = budgetCtrl.getBudget();
         
         //3. Display the budget on the UI
-        console.log(budget);
+        uiCtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function() {
-        var inputData, newItem;
+        var input, newItem;
 
         //1. Get the filled input data
-        inputData = uiCtrl.getInputValues();
+        input = uiCtrl.getInput();
     
         //Only happens if there is any data
-        if(input.description !== "" && !isNan(input.value) && input.value > 0) {
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0) {
             //2. Add new items to the budget controller
-            newItem = budgetCtrl.addItem(inputData.inputType, inputData.inputDescription, inputData.inputValue);
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
             //3. Add new items to the UI
-            uiCtrl.addItemUI(newItem, inputData.type);
+            uiCtrl.addListItem(newItem, input.type);
 
             //4. Clear the fields
             uiCtrl.clearFields();   
         
             //5. Calculate and update budget
             updateBudget();
-        }
+        } 
+    };
+
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID;
+
+        //accessing the parent nodes using reversing/traversing
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        //split the string using hyphen as the center to two
+        splitID = itemID.split('-');
+        type = spltiID[0];
+        ID = splitID[1];
+
+        //1. Delete the item from the data structure
+
+        //2. Delete the item from the UI
+
+        //3. Update and show the new budget
         
     };
 
     //PUBLIC VARIABLES AND FUNCTIONS
     return {
         init: function() {
+            console.log('Application has started!');
+            uiCtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     }
@@ -300,3 +358,24 @@ var controller = (function(budgetCtrl, uiCtrl){
 
 //call the init function to activate all the event listeners
 controller.init();
+
+
+
+/********************************NOTES**************************************/
+
+/*
+    EVENT DELEGATIONS
+    1. Event Bubbling
+        When you call an event, for example like a button click, it triggers all the
+        parent elements and upto to the html element, which is the root.
+    2. Event Target
+        In this case, the button will be the event target, since it is the root for
+        triggering all the other elements of the HTML DOM.
+    3. Event Delegates
+        So while triggering an event like this, we can setup and trigger other functions/events
+        in any parts of the document, since it triggers the entire document.
+    4. Use cases of EVENT DELEGATIONS
+        a) When we have an element with lots of child elements that we are interested in.
+        b) When we want an event handler attached to an element that is not yet in the DOM
+            when our page is loaded.
+*/
